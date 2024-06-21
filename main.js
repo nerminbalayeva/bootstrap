60bc4ce8e86fc155ee3c68c794ed3e8b983d02df
nerminbalayeva <nerminbalayeva1@icloud.com''git config --global github.user nerminbalayeva>
2024-06-21 15:24:23 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// 1. pop, push, unshift, shift ve concat ile bir numune yazirsiz //eldik
+// 2. gunlerin reqemlerine uyqun olaraq netice verirsiz meselen (1=bazar ertesi) switch case ile
+// 3. istifadeciden bir deyer aliriq ve deyeri arraya elave edirik //eledik
+// 4. iceririsnde reqemler olan arraydan ilk 3 reqemi silirik. //eledik
+// 4. arrayda olan sonuncu elementi tutub diger list-e elave edib log-da gosteririk. // eledik
+// 5. bir array yaradiriq ve arayin uzunluqunu log-da yaziriq. //eledik
+// 5. promptdan deyer elde edib arraya elave edirik. //eledik
+// 6. switch case ile kalkulyator yazirsiz
+
+
+// var array1 =[1,2,3,4,5,"nermin"]
+// var array2 =[]
+
+// console.log(array1[array1.length-1]);
+// console.log(array2.push(array1[array1.length-1]));
+// console.log(array2);
+
+// var cavab = prompt("melumat qeyd edin")
+
+// var array = []
+// array.push(cavab);
+
+// console.log(array);
+
+// var array = [1,2,3,4,5,6,7]
+// console.log(array);
+// array.shift()
+// array.shift()
+// array.shift()
+// console.log(array);
